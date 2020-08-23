f0eb5b88738ad56b6843ab3e8fe2b3d482668d6e
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2020-08-23 16:31:24 +0200

    feat: First draft of the main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,50 @@ export default function Home() {
   return (
     <>
       <Head title='threkk.com' description='Hello world' url='/' image='' />
-      <p>Hello world</p>
+      <div className='bg-secondary'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column col-mx-auto col-8 col-md-10 col-sm-12'>
+              <main className='hero'>
+                <div className='hero-body'>
+                  <figure
+                    className='avatar avatar-xl'
+                    style={{ height: '6.4rem', width: '6.4rem' }}
+                  >
+                    <img src='/avatar.jpg' alt='My interenet avatar' />
+                  </figure>
+                  <h1>Hi, my name is Alberto</h1>
+                  <mark>a.k.a threkk</mark>
+                  <p className='py-1 text-large'>
+                    I am a Software Engineer based in <strong>Amsterdam</strong>{' '}
+                    focused on creating solutions mixing the right amount of
+                    cutting edge innovations with solid stable technologies
+                    focusing on maintainability and portability.
+                  </p>
+                  <p className='py-1'>On my free time...</p>
+                </div>
+                <div>
+                  <span className='label label-rounded label-primary m-1'>
+                    JavaScript
+                  </span>
+                  <span className='label label-rounded label-primary m-1'>
+                    Python
+                  </span>
+                  <span className='label label-rounded label-primary m-1'>
+                    Golang
+                  </span>
+                  <span className='label label-rounded label-primary m-1'>
+                    AWS
+                  </span>
+                  <span className='label label-rounded label-primary m-1'>
+                    Kubernetes
+                  </span>
+                </div>
+              </main>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
