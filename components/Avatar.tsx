cf84cc68d0414eb5271fdde430f3073a25f8fe91
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2021-10-10 17:58:13 +0200

    feat: Updates the avatar component

---
+++ components/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import avatarBW from '../public/avatar/avatar_bw.jpg'
+
+export default function Avatar () {
+  const width = 128
+  const height = Math.ceil(width / (3 / 4))
+  const image = <Image
+        className='is-rounded u-photo'
+        src={avatarBW}
+        width={width}
+        height={height}
+        alt='Yup, that is me'
+        />
+
+  return (
+      <figure className='image' style={{width, height}}>
+        {image}
+      </figure>
+  )
+}
