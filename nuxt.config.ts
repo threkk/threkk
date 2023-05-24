4b6d2223a7aea1effc91d3fd58cae3d37ccd9fe8
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2023-05-25 00:26:03 +0200

    feat: plugins added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxt/image-edge',
+    '@nuxt/devtools',
+    'nuxt-icon',
+    'nuxt-cloudflare-analytics',
+    '@nuxt/content',
+    // Enable only in production or it will break the dev tools
+    // 'nuxt-security', 
+    '@vueuse/nuxt'
+  ],
+  extends: [
+    // TODO: configure seo kit https://nuxt.com/modules/seo-kit
+    'nuxt-seo-kit'
+  ],
+  devtools: {
+    enabled: true
+  },
+  cloudflareAnalytics: {
+    // https://nuxt.com/modules/cloudflare-analytics
+  },
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
 })
