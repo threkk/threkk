398b23eba5315841e5127c92463c145407edf6da
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2023-06-01 00:54:29 +0200

    feat: loads css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const extraModules = []
+if (process.env.NODE_ENV === 'development') {
+  extraModules.push('@nuxt/devtools')
+} else if (process.env.NODE_ENV === 'production') {
+  extraModules.push('nuxt-security')
+}
+
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/eslint-module',
     '@nuxt/image-edge',
-    '@nuxt/devtools',
     'nuxt-icon',
     'nuxt-cloudflare-analytics',
     '@nuxt/content',
-    // Enable only in production or it will break the dev tools
-    // 'nuxt-security', 
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    ...extraModules
   ],
   extends: [
     // TODO: configure seo kit https://nuxt.com/modules/seo-kit
     'nuxt-seo-kit'
+  ],
+  css: [
+    '@/assets/theme.css',
+    '@/assets/inter.css',
+    '@/assets/fira_code.css',
+    '@picocss/pico/css/pico.css'
   ],
   devtools: {
     enabled: true
